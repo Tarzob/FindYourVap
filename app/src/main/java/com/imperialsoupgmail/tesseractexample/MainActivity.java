dd8efcd202ad1b054f5d55639bf21980d81741b4
yasmine <yasmine.bennadir@gmail.com>
2016-11-16 14:32:31 +0100

    main activity

--- app/src/main/java/com/imperialsoupgmail/tesseractexample/MainActivity.java
+++ app/src/main/java/com/imperialsoupgmail/tesseractexample/MainActivity.java
@@ -1,18 +1,21 @@
 package com.imperialsoupgmail.tesseractexample;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
@@ -24,43 +27,76 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
 
-    Bitmap image;
-    private TessBaseAPI mTess;
     String datapath = "";
+    private TessBaseAPI mTess;
+    Bitmap image;
+
+    ImageButton logoButton;
+    ImageView imageView;
+    ImageButton goButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //init image
-        image = BitmapFactory.decodeResource(getResources(), R.drawable.test_image);
+        imageView = (ImageView)this.findViewById(R.id.imageView);
+        goButton=(ImageButton) findViewById(R.id.goButton) ;
+        logoButton = (ImageButton) findViewById(R.id.logoButton);
+
+        imageView .setVisibility(View.INVISIBLE);
+        goButton.setVisibility(View.INVISIBLE);
+
+
+        logoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            photo=getDropShadow3(photo);
+            imageView.setImageBitmap(photo);
+            imageView.setVisibility(View.VISIBLE);
+            goButton.setVisibility(View.VISIBLE);
+            logoButton.setVisibility(View.GONE);
+        }
+    }
+    public void processImage(View view){
 
         //initialize Tesseract API
         String language = "eng";
-        datapath = getFilesDir() + "/tesseract/";
+        datapath = getFilesDir()+ "/tesseract/";
         mTess = new TessBaseAPI();
-
         checkFile(new File(datapath + "tessdata/"));
-
         mTess.init(datapath, language);
-    }
 
-    public void processImage(View view) {
-        String OCRresult = null;
+        String result = null;
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        image = drawable.getBitmap();
         mTess.setImage(image);
-        OCRresult = mTess.getUTF8Text();
-        TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
-        OCRTextView.setText(OCRresult);
+        result = mTess.getUTF8Text();
+        result = result.replaceAll("\\s+","");
+        Log.e("mon Résultat est :",result);
+        Intent i=new Intent(this, InfoActivity.class);
+        i.putExtra("nameOfLiquid",result);
+        startActivity(i);
     }
-
     private void checkFile(File dir) {
-        if (!dir.exists() && dir.mkdirs()) {
+        if (!dir.exists()&& dir.mkdirs()){
             copyFiles();
         }
-        if (dir.exists()) {
-            String datafilepath = datapath + "/tessdata/eng.traineddata";
+        if(dir.exists()) {
+            String datafilepath = datapath+ "/tessdata/eng.traineddata";
             File datafile = new File(datafilepath);
 
             if (!datafile.exists()) {
@@ -97,5 +133,42 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    private static Bitmap getDropShadow3(Bitmap bitmap) {
+
+        if (bitmap==null) return null;
+        int think = 6;
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+
+        int newW = w - (think);
+        int newH = h - (think);
+
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+        Bitmap bmp = Bitmap.createBitmap(w, h, conf);
+        Bitmap sbmp = Bitmap.createScaledBitmap(bitmap, newW, newH, false);
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        Canvas c = new Canvas(bmp);
+
+        // Right
+        Shader rshader = new LinearGradient(newW, 0, w, 0, Color.GRAY, Color.LTGRAY, Shader.TileMode.CLAMP);
+        paint.setShader(rshader);
+        c.drawRect(newW, think, w, newH, paint);
+
+        // Bottom
+        Shader bshader = new LinearGradient(0, newH, 0, h, Color.GRAY, Color.LTGRAY, Shader.TileMode.CLAMP);
+        paint.setShader(bshader);
+        c.drawRect(think, newH, newW  , h, paint);
+
+        //Corner
+        Shader cchader = new LinearGradient(0, newH, 0, h, Color.LTGRAY, Color.LTGRAY, Shader.TileMode.CLAMP);
+        paint.setShader(cchader);
+        c.drawRect(newW, newH, w  , h, paint);
+
+
+        c.drawBitmap(sbmp, 0, 0, null);
+
+        return bmp;
     }
 }
